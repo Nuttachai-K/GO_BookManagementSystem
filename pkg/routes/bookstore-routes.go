700adc96850a70161aea39d046709b71785880e3
Korn <korn_12@DESKTOP-E9SDIHD.localdomain>
2022-12-26 12:50:19 +0700

    fix Get by id error 404 not found

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -8,7 +8,7 @@ import (
 var RegisterBookStoreRoutes = func(router *gin.Engine) {
 	router.POST("/book/", controllers.CreateBook)
 	router.GET("/book", controllers.GetBook)
-	router.GET("/book/{bookId}", controllers.GetBookById)
-	router.PUT("/book/{bookId}", controllers.UpdateBook)
-	router.DELETE("/book/{bookId}", controllers.DeleteBook)
+	router.GET("/book/:bookId", controllers.GetBookById)
+	router.PUT("/book/:bookId", controllers.UpdateBook)
+	router.DELETE("/book/:bookId", controllers.DeleteBook)
 }
